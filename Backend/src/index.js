86e214b25927207da1fe00174b1cff3d914f934f
Swapnil190623 <swapnilathavale169@gmail.com>
2024-08-27 15:46:09 +0530

    add initial files & Folder Structure

---
+++ Backend/src/index.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+import connectDB from "./db/db.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB()
+  
---
+++ Backend/src/utils/firebase.js
@@ -0,0 +1,10 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('./path-to-your-service-account-key.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+const auth = admin.auth();
+
+module.exports = { auth };
